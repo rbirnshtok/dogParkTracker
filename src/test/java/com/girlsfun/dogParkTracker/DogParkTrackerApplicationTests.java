e73730a500226dfcd7332c5818cf68e41186b82a
rbirnshtok <rbirnshtok@agilissystems.com>
2022-01-11 16:49:15 -0600

    initial commit

---
+++ src/test/java/com/girlsfun/dogParkTracker/DogParkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girlsfun.dogParkTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogParkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
